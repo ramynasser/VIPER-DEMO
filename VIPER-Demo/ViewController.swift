08bad7105b4ea21cdbb9b4929511930fecab109f
Ramy Nasser <ramy.assr@vodafone.com>
2020-09-27 17:10:32 +0200

    Initial Commit

---
+++ VIPER-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIPER-Demo
+//
+//  Created by Ramy Nasser on 9/27/20.
+//  Copyright © 2020 Ramy Nasser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
